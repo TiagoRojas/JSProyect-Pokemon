ffedc66c1ce9d9ffadb4d90da5aca2cc0915028a
Tiago Rojas <rojastiago04@gmail.com>
2022-07-16 20:29:23 -0300

    Added Sprites and Codded JS

--- js/main.js
+++ js/main.js
@@ -1,9 +1,41 @@
 function run() {
-  document.body.innerHTML = '<video src="assets/gameintro.mp4" width="960" height="640" autoplay="auto" loop volume="0.1"></video>';
+  document.body.innerHTML = '<video src="assets/videos/gameintro.mp4" width="960" height="640" autoplay="auto" loop id="video" onclick="skipintro()"></video>';
+  const video = document.getElementById("video")
+  video.volume = 0.2
+  video.addEventListener("timeupdate", function() {
+    if (this.currentTime >= 7.5) {
+      video.setAttribute("onClick", "startgame()");
+    }
+  });
+}
+
+function skipintro() {
+  const video = document.getElementById("video");
+  const videotime = video.currentTime
+  if (videotime > 7.5) {
+    video.currentTime = 20.5;
+    video.setAttribute("onClick", "startgame()");
+  }
+}
+
+function startgame() {
+  const video = document.getElementById("video");
+  const videotime = video.currentTime
+  if (videotime > 20.5) {
+    video.remove()
+  } else {
+    video.currentTime = 20.5;
+  }
 }
 
 
-const numeros = [1, 2, 3, 4, 5, 6]
-const total = numeros.reduce((a, el) => a + el * 20 / 100, 0)
 
-console.log(total)
+
+let numero1 = 2
+let numero2 = 3
+
+if (numero1 > numero2) {
+  console.log("yes")
+} else {
+  console.log("no")
+}
